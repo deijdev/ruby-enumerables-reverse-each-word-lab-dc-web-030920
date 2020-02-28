85409c325e9ae47752d27dd95d389e71b1c0e4d4
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 03:47:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(string)
   string= string.split("")
   
   string_1= [" "]
-  string_1.each do |n|
-  text = "Current number is: #{n}"
+  text=string_1.each do |n|
+  text_2 = "Current number is: #{n}"
   puts text
 end
 end

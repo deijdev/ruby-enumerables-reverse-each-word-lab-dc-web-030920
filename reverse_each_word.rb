194a83198a1032bfb4b6f7f1b8d0e2ca56188b97
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 04:03:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,14 +4,7 @@ def reverse_each_word(string)
   return reversed_string.join(" ")
 end
 
-# FIRST REFACTORED CODE
-def reverse_each_word(array)
-  new_array = array.split(" ")
-  new_array.collect {|x| x.reverse!}
-  new_array.join(" ")
-end
 
-# FINAL SOLUTION
-def reverse_each_word(array)
-  array.split(" ").collect {|x| x.reverse!}.join(" ")
+def reverse_each_word(string)
+  string.split(" ").collect {|x| x.reverse!}.join(" ")
 end
